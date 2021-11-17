2707deff6e6e91b7abea19f46188320c059ac33b
Anchit Mishra <champu.1729@icloud.com>
2021-11-17 20:29:53 +0800

    Minor fixes (6) - Final

--- faces.py
+++ faces.py
@@ -10,7 +10,7 @@ def faces():
     import sys
 
     # 1 Create database connection
-    myconn = mysql.connector.connect(host="localhost", user="root", passwd="Raghav@2001", database="banking_application")
+    myconn = mysql.connector.connect(host="localhost", user="root", passwd="root1234", database="banking_application")
     date = datetime.utcnow()
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
